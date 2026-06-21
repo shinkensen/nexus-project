eaadd4258d0676db9a16267cc752e3da4305e620
Shinkensen <noobywinner587@gmail.com>
2026-06-21 04:39:34 -0700

    s

--- app/controller/page.tsx
+++ app/controller/page.tsx
@@ -434,7 +434,7 @@ export default function Controller() {
                 style={{
                     height: "100vh",
                     overflow: "hidden",
-                    backgroundColor: "#20b14c",
+                    backgroundColor: isMobile ? "#20b14c" : "#000",
                     backgroundImage: isMobile ? `url(${backgroundImage.src})` : "none",
                     backgroundSize: isMobile ? "contain" : "auto",
                     backgroundPosition: "center center",
